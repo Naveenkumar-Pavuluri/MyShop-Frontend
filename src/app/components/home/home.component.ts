28e0c77ae11951fc8aa0d287b5815fa306e2470b
Naveen Kumar Pavuluri <naveenpavuluri0910@gmail.com>
2025-03-23 22:53:57 +0530

    added responsive tailwind css for all screen sizes

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -11,36 +11,47 @@ import { CartService } from '../../services/cart.service';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CarouselModule,RouterLink,MatButtonModule,ProductCardComponent],
+  imports: [CarouselModule, RouterLink, MatButtonModule, ProductCardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
   customerService = inject(CustomerService);
-  newProducts:Product[]=[];
-  featuredProducts:Product[]=[];
-  bangerImages:Product[]=[]
+  newProducts: Product[] = [];
+  featuredProducts: Product[] = [];
+  bangerImages: Product[] = []
   wishlistService = inject(WishlistService);
   cartService = inject(CartService);
   customOptions: OwlOptions = {
     loop: true,
-    mouseDrag: false,
-    touchDrag: false,
-    pullDrag: false,
-    dots: false,
-    navSpeed: 700,
-    navText: ['', ''],
-    nav: true,
-};
+    margin: 10, 
+    nav: false,
+    dots: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      480: {
+        items: 2
+      },
+      768: {
+        items: 3 
+      },
+      1024: {
+        items: 4 
+      }
+    }
+  };
 
-  
-  ngOnInit(){
-    this.customerService.getNewProducts().subscribe((result)=>{
+
+
+  ngOnInit() {
+    this.customerService.getNewProducts().subscribe((result) => {
       this.newProducts = result;
       this.bangerImages.push(...result)
     });
 
-    this.customerService.getFeaturedProducts().subscribe((result)=>{
+    this.customerService.getFeaturedProducts().subscribe((result) => {
       this.featuredProducts = result;
       this.bangerImages.push(...result)
     });
